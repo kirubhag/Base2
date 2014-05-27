b7f54c69e181c246f6ba3efe528487591843b73b
kirubhag <kirubhag@gmail.com>
2014-05-27 17:43:49 +0530

    Test

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once 'libs/Config.php';
+include_once 'libs/DB.php';
+include_once 'libs/Utilities.php';
+include_once 'libs/Bootstrap.php';
+include_once 'libs/Router.php';
+
+$apps = new Router();
